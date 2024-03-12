e1ceeb7a7a910444e36cb5dc78b74f050b4974f9
Vasista R <96642595+VasistaR@users.noreply.github.com>
2024-03-12 09:09:24 -0700

    minor schedule and about section changes

--- app/src/components/sections/About.jsx
+++ app/src/components/sections/About.jsx
@@ -15,8 +15,8 @@ const About = () => {
           >
             <path d="M280-120v-80h160v-124q-49-11-87.5-41.5T296-442q-75-9-125.5-65.5T120-640v-40q0-33 23.5-56.5T200-760h80v-80h400v80h80q33 0 56.5 23.5T840-680v40q0 76-50.5 132.5T664-442q-18 46-56.5 76.5T520-324v124h160v80H280Zm0-408v-152h-80v40q0 38 22 68.5t58 43.5Zm200 128q50 0 85-35t35-85v-240H360v240q0 50 35 85t85 35Zm200-128q36-13 58-43.5t22-68.5v-40h-80v152Zm-200-52Z" />
           </svg>
-          <h2 className="font-bold">1st Place Prize</h2>
-          <h3>19 Dollah Fortnite Card</h3>
+          <h2 className="font-bold">Beginner Friendly</h2>
+          <h3>All high schoolers regardless of coding experience can have fun!</h3>
         </div>
         <div className="flex flex-col gap-2 p-3 bg-gray-800 rounded-md w-full">
           <svg
@@ -28,8 +28,8 @@ const About = () => {
           >
             <path d="M280-120v-80h160v-124q-49-11-87.5-41.5T296-442q-75-9-125.5-65.5T120-640v-40q0-33 23.5-56.5T200-760h80v-80h400v80h80q33 0 56.5 23.5T840-680v40q0 76-50.5 132.5T664-442q-18 46-56.5 76.5T520-324v124h160v80H280Zm0-408v-152h-80v40q0 38 22 68.5t58 43.5Zm200 128q50 0 85-35t35-85v-240H360v240q0 50 35 85t85 35Zm200-128q36-13 58-43.5t22-68.5v-40h-80v152Zm-200-52Z" />
           </svg>
-          <h2 className="font-bold">1st Place Prize</h2>
-          <h3>19 Dollah Fortnite Card</h3>
+          <h2 className="font-bold">Fully catered</h2>
+          <h3>No need to bring lunch or dinner! Our sponsors have generously covered food for everyone!</h3>
         </div>
         <div className="flex flex-col gap-2 p-3 bg-gray-800 rounded-md w-full">
           <svg
@@ -41,8 +41,8 @@ const About = () => {
           >
             <path d="M280-120v-80h160v-124q-49-11-87.5-41.5T296-442q-75-9-125.5-65.5T120-640v-40q0-33 23.5-56.5T200-760h80v-80h400v80h80q33 0 56.5 23.5T840-680v40q0 76-50.5 132.5T664-442q-18 46-56.5 76.5T520-324v124h160v80H280Zm0-408v-152h-80v40q0 38 22 68.5t58 43.5Zm200 128q50 0 85-35t35-85v-240H360v240q0 50 35 85t85 35Zm200-128q36-13 58-43.5t22-68.5v-40h-80v152Zm-200-52Z" />
           </svg>
-          <h2 className="font-bold">1st Place Prize</h2>
-          <h3>19 Dollah Fortnite Card</h3>
+          <h2 className="font-bold">Prizes</h2>
+          <h3>Over $TBD in prizes!</h3>
         </div>
       </div>
     </div>
