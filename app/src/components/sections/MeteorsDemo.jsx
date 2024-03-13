c0e94507fe4e5cf79f258ff887bd8583752949c8
Vasista R <vasista.pioneer@gmail.com>
2024-03-12 23:17:47 -0700

    Update MeteorsDemo.jsx

--- app/src/components/sections/MeteorsDemo.jsx
+++ app/src/components/sections/MeteorsDemo.jsx
@@ -46,7 +46,7 @@ export function MeteorsDemo() {
                 Milpitas Hacks
               </h1>
               <div className="flex justify-between gap-10">
-                <h2 className="text-yellow-200">May 23, 2024</h2>
+                <h2 className="text-yellow-200">May 25, 2024</h2>
                 <a href="https://www.google.com/maps/place/Milpitas+High+School/@37.4509418,-121.901557,18.63z/data=!4m6!3m5!1s0x808fcf3566e07d13:0x8cb806b94146f8!8m2!3d37.4509315!4d-121.9005301!16zL20vMDVjanI0?entry=ttu" target="_blank" className="hover:underline text-yellow-200">@ Milpitas High School</a>
               </div>
             </div>
