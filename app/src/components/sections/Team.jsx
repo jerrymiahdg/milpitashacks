f816a978a2179df5c8c7b9b8fa97843bc17f428b
devmam999 <devmam999@gmail.com>
2024-03-14 18:52:35 -0700

    attempt to center logo

--- app/src/components/sections/Team.jsx
+++ app/src/components/sections/Team.jsx
@@ -40,7 +40,7 @@ const Team = () => {
               </CardItem>
               <CardItem
                 translateZ={20}
-                className="py-2 rounded-xl text-xs font-normal dark:text-white align-items-center"
+                className="py-2 rounded-xl text-xs font-normal dark:text-white flex justify-center items-center h-screen"
               >
                 <a href="https://www.linkedin.com/in/aahan-shah/" target="_blank"><img
                   src={linkedInPng}
