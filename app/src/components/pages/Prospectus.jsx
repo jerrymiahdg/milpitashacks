8ed22536f54919483181716f219fcac4ee8e984d
devmam999 <devmam999@gmail.com>
2024-03-16 12:52:28 -0700

    doing some positioning

--- app/src/components/pages/Prospectus.jsx
+++ app/src/components/pages/Prospectus.jsx
@@ -81,10 +81,10 @@ const Prospectus = () => {
         </div>
         <h1 className="text-lg">*all number of participants are estimates</h1>
       </div>
-      <ProspectusFAQ />
       <div className="flex flex-col items-center p-5">
       <h1 className="text-lg">Interested in sponsoring us? Contact us at <a href="mailto:milpitashacks@gmail.com" className="text-blue-200 hover:underline">milpitashacks@gmail.com</a></h1>
       </div>
+      <ProspectusFAQ />
     </div>
   );
 };
