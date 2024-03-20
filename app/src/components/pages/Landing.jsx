b51979e05c2a09f63c1630d8d411ee8f11bcd0fd
devmam999 <devmam999@gmail.com>
2024-03-19 19:40:08 -0700

    style attempt

--- app/src/components/pages/Landing.jsx
+++ app/src/components/pages/Landing.jsx
@@ -57,10 +57,10 @@ const Landing = ({
             <Button>Register</Button>
           </a>
         </div>
-        <h2 className="text-center leading-7">
+      </div>
+      <h2 className="text-center leading-7">
       Milpitas Hacks is fiscally sponsored by The Hack Foundation (d.b.a. Hack Club), 501(c)(3), (EIN: 81-2908499).
       </h2>
-      </div>
     </>
   );
 };
