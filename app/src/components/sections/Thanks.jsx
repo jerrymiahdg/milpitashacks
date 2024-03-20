69317cf7702732cca7fd5dc1d58e0c56a848e108
devmam999 <devmam999@gmail.com>
2024-03-19 17:49:28 -0700

    styling attempt

--- app/src/components/sections/Thanks.jsx
+++ app/src/components/sections/Thanks.jsx
@@ -1,6 +1,6 @@
 const Thanks = () => {
     return (
-      <div className="flex flex-col items-center py-20 px-5  bg-gradient-to-t from-gray-950 bg-primary">
+      <div className="flex flex-col items-center bg-gradient-to-b from-gray-950 bg-primary pt-20 pb-20 pl-5 pr-5">
       <div className="pb-20">
         <h1 className="text-4xl font-bold text-yellow-200">Special Thanks</h1>
       </div>
