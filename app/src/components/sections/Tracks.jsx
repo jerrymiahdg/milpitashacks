6cd55426f8ff10853e712377e357020c7e438974
jerrymiahdg <kingbebop15@gmail.com>
2024-03-25 21:32:17 -0700

    updated responsivity of tracks section

--- app/src/components/sections/Tracks.jsx
+++ app/src/components/sections/Tracks.jsx
@@ -4,31 +4,33 @@ const Tracks = () => {
       <div className="pb-20">
         <h1 className="text-4xl font-bold">Tracks</h1>
       </div>
-      <div className="flex md:flex-row">
-      <div className="flex gap-5">
-        <div className="flex flex-col gap-10 p-10 bg-gray-950/50 rounded-lg mr-5">
-          <h2 className="font-bold text-3xl sm:text-5xl text-yellow-200">
+      <div className="flex max-w-7xl w-full flex-col lg:flex-row gap-5">
+        <div className="flex flex-col gap-10 p-10 bg-gray-950/50 rounded-lg">
+          <h2 className="font-bold text-3xl sm:text-4xl text-yellow-200">
             Education
           </h2>
-          <h3 className="text-lg">Build an education website or app that helps users get educated</h3>
+          <h3 className="text-lg">
+            Build an education website or app that helps users get educated
+          </h3>
         </div>
-      </div>
-      <div className="flex gap-5">
         <div className="flex flex-col gap-10 p-10 bg-gray-950/50 rounded-lg">
-          <h2 className="font-bold text-3xl sm:text-5xl text-yellow-200">
+          <h2 className="font-bold text-3xl sm:text-4xl text-yellow-200">
             Entertainment and Social Media
           </h2>
-          <h3 className="text-lg">Build an entertaining application/website or a social media website/app</h3>
+          <h3 className="text-lg">
+            Build an entertaining application/website or a social media
+            website/app
+          </h3>
         </div>
-      </div>
-      <div className="flex gap-5">
-        <div className="flex flex-col gap-10 p-10 bg-gray-950/50 rounded-lg ml-5">
-          <h2 className="font-bold text-3xl sm:text-5xl text-yellow-200">
+        <div className="flex flex-col gap-10 p-10 bg-gray-950/50 rounded-lg">
+          <h2 className="font-bold text-3xl sm:text-4xl text-yellow-200">
             Assistive Technologies
           </h2>
-          <h3 className="text-lg">Build a technology to help people with disabilities such as blindess and hearing loss</h3>
+          <h3 className="text-lg">
+            Build a technology to help people with disabilities such as blindess
+            and hearing loss
+          </h3>
         </div>
-      </div>
       </div>
     </div>
   );
