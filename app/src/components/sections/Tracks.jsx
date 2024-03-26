7e3b37fc5abc55eddbf3cdfde03f59ee7c9b3115
devmam999 <devmam999@gmail.com>
2024-03-25 21:06:48 -0700

    another attempt

--- app/src/components/sections/Tracks.jsx
+++ app/src/components/sections/Tracks.jsx
@@ -1,9 +1,10 @@
 const Tracks = () => {
   return (
-    <div className="flex flex-col items-center bg-gradient-to-b from-gray-950 bg-primary pt-20 pb-20 pl-5 pr-5 md:flex-row">
+    <div className="flex flex-col items-center bg-gradient-to-b from-gray-950 bg-primary pt-20 pb-20 pl-5 pr-5">
       <div className="pb-20">
         <h1 className="text-4xl font-bold">Tracks</h1>
       </div>
+      <div className="md:flex-row">
       <div className="flex gap-5">
         <div className="flex flex-col gap-10 p-10 bg-gray-950/50 rounded-lg mr-5">
           <h2 className="font-bold text-3xl sm:text-5xl text-yellow-200">
@@ -27,6 +28,7 @@ const Tracks = () => {
           </h2>
           <h3 className="text-lg">Build a technology to help people with disabilities such as blindess and hearing loss</h3>
         </div>
+      </div>
       </div>
     </div>
   );
