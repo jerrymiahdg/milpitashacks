bef6a5e4f4055c518e47cfe922e7406aba87395e
devmam999 <devmam999@gmail.com>
2024-03-25 20:54:40 -0700

    added tracks

--- app/src/components/sections/Tracks.jsx
+++ app/src/components/sections/Tracks.jsx
@@ -5,11 +5,27 @@ const Tracks = () => {
         <h1 className="text-4xl font-bold">Tracks</h1>
       </div>
       <div className="flex gap-5">
+        <div className="flex flex-col gap-10 p-10 bg-gray-950/50 rounded-lg mr-5">
+          <h2 className="font-bold text-3xl sm:text-5xl text-yellow-200">
+            Education
+          </h2>
+          <h3 className="text-lg">Build an education website or app that helps users get educated</h3>
+        </div>
+      </div>
+      <div className="flex gap-5">
         <div className="flex flex-col gap-10 p-10 bg-gray-950/50 rounded-lg">
           <h2 className="font-bold text-3xl sm:text-5xl text-yellow-200">
-            To be announced...
+            Entertainment and Social Media
           </h2>
-          <h3 className="text-lg">To be announced...</h3>
+          <h3 className="text-lg">Build an entertaining application/website or a social media website/app</h3>
+        </div>
+      </div>
+      <div className="flex gap-5">
+        <div className="flex flex-col gap-10 p-10 bg-gray-950/50 rounded-lg ml-5">
+          <h2 className="font-bold text-3xl sm:text-5xl text-yellow-200">
+            Assistive Technologies
+          </h2>
+          <h3 className="text-lg">Build a technology to help people with disabilities such as blindess and hearing loss</h3>
         </div>
       </div>
     </div>
