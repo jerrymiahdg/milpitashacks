19111ddb1e2752bd5eab11a5d7246cfcf57f58aa
oliverma-california <om86052@yahoo.com>
2024-05-03 20:22:15 -0700

    prizes

--- app/src/components/sections/About.jsx
+++ app/src/components/sections/About.jsx
@@ -1,5 +1,9 @@
 import { GraduationCap, Utensils, Trophy } from "lucide-react";
 
+// lmfao
+const prizes = 18;
+const prizeAmt = 1500;
+
 const About = () => {
   return (
     <div className="flex flex-col items-center bg-gradient-to-b from-gray-950 to-gray-900 pt-20 pb-40 pl-5 pr-5">
@@ -22,7 +26,7 @@ const About = () => {
         </div>
         <div className="flex flex-col p-10 bg-gray-950/50 rounded-md w-full transition-all shadow-[0_0px_50px_-15px_rgba(0,0,0,0.3)] hover:shadow-[0_0px_100px_-15px_rgba(0,0,0,0.3)] shadow-indigo-800 hover:shadow-indigo-500">
           <h2 className="font-bold text-3xl inline-flex gap-5 py-3"><Trophy size={35} />Prizes</h2>
-          <h3 className="font-normal text-lg">Over $TBD in prizes!</h3>
+          <h3 className="font-normal text-lg">{prizes} prizes, including overall and track-specific awards, worth over ${prizeAmt} in value!</h3>
         </div>
       </div>
     </div>
